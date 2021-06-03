14a046a3ef3de41145686cfe49c46d08edac1db6
SameerSS19 <sk3090019@gmail.com>
2021-06-03 21:57:46 +0530

    [Sameer]Add:Handle Exception if User Provides Invalid Mood

--- MoodAnalyzer.java
+++ MoodAnalyzer.java
@@ -1,18 +1,29 @@
 package com.moodanalyser;
 
-public class MoodAnalyzer {
-		String message;
-	    public MoodAnalyzer(String message) {
-	        this.message = message;
-	    }
+public class MoodAnalyzer 
+{
+	String message;
+    public MoodAnalyzer(String message) 
+    {
+        this.message = message;
+    }
 
-	    public String analyzeMood(String message){
-	        this.message = message;
-	        return analyzeMood();
-	    }
-	    public String analyzeMood(){
-	        if (message.contains("sad"))
-	            return "SAD";
-	        return "HAPPY";
-	    }
+    public String analyzeMood(String message)
+    {
+        this.message = message;
+        return analyzeMood();
+    }
+    public String analyzeMood() 
+    {
+        try 
+        {
+        if (message.contains("sad"))
+            return "SAD";
+        return "HAPPY";
+        } 
+        catch (NullPointerException e) 
+        {
+            return "HAPPY";
+        }
+     }
 }
--- MoodAnalyzerTest.java
+++ MoodAnalyzerTest.java
@@ -1,27 +1,32 @@
 package com.moodanalyser;
-
-import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.Assert;
 
 public class MoodAnalyzerTest {
 
 	@Test
-    public void givenSadMessage() {
+    public void givenSadMessageShouldReturnSadMessage() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("Hello am in sad mood");
         String mood = moodAnalyzer.analyzeMood();
         Assert.assertEquals("SAD", mood);
     }
     @Test
-    public void givenHappyMassage(){
+    public void givenHappyMassageShouldReturnHappyMessage(){
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("Hello am in happy mood");
         String mood = moodAnalyzer.analyzeMood();
         Assert.assertEquals("HAPPY", mood);
     }
     @Test
-    public void givenAnyMassage() {
+    public void givenAnyMassageShouldReturnHappyMessage() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("Hello am not in mood");
         String mood = moodAnalyzer.analyzeMood();
         Assert.assertEquals("HAPPY", mood);
+    }
+    @Test
+    public void givenNullMassageShouldReturnHappyMessage(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        String mood = moodAnalyzer.analyzeMood();
+        Assert.assertEquals("HAPPY",mood);
+
     }
 }
